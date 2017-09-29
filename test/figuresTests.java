2e0969474f755a723cdb1ddd2c7b80d0abf47098
IskanderNakipov <iskander.sych@gmail.com>
2017-09-29 17:14:58 +0300

    Tests passed

--- test/figuresTests.java
+++ test/figuresTests.java
@@ -15,6 +15,23 @@ public class figuresTests {
 
     @Test
     public  void testRectangle() {
+        double a = 2.0;
+        double b = 3.0;
+        Rectangle A = new Rectangle();
+        A.a = a;
+        A.b = b;
+        assertEquals(a*b, A.area(),0.01);
+    }
+    @Test
+    public  void testTriangle() {
+        double a = 3.0;
+        double b = 4.0;
+        double c = 5.0;
 
+        Triangle A = new Triangle();
+        A.a = a;
+        A.b = b;
+        A.c = c;
+        assertEquals(a*b*0.5, A.area(),0.01);
     }
 }
